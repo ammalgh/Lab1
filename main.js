94e09e1567178cef0fd4fa5224786b62981c4193
ammalgh <ammalgh22@gmail.com>
2024-05-21 18:44:30 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+var x= 6;
+var y=3
+result = x*y;
+console.log(result);
+
+
+// q2
+var tem =30;
+var F= tem*1.8+32;
+console.log(F);
+var c= F/1.8-32;
+console.log(c);
+
+
+//q3
+var hour=4;
+var res= hour*3600;
+console.log(hour+"hour = " +res +"secound" ) ;
+
+//q6
