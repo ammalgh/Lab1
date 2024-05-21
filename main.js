4c58118dd30081dd17e964b69128517b34a546ac
ammalgh <ammalgh22@gmail.com>
2024-05-21 20:48:56 +0300

    first commit

--- main.js
+++ main.js
@@ -84,3 +84,5 @@ var toppings = "sprinkles";
 if ((flavor === "vanilla" || flavor === "chocolate") && (vessel === "cone" || vessel === "bowl") && (toppings === "sprinkles" || toppings === "peanuts")) {
   console.log("I'd like two scoops of " + flavor + " ice cream in a " + vessel + " with " + toppings + ".");
 }
+
+//lab2
